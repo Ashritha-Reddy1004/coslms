3ea4c10eb10a758c475170700bc17dcc8ce14cda
ashritha <ashrithareddy1004@gmail.com>
2023-03-08 15:14:14 +0530

    feat:added leave-status query

--- x/lms/client/cli/query.go
+++ x/lms/client/cli/query.go
@@ -51,9 +51,10 @@ func GetQueryCmd() *cobra.Command {
 // Get leave command
 func GetLeavesCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "accepted-leaves",
-		Short: "List all applied leaves",
-		Long:  `List of all the applied leaves by the students`,
+		Use:     "applied-leaves",
+		Short:   "Applied Leaves",
+		Long:    `List of all the applied leaves by the students`,
+		Example: "./simd query coslms applied-leaves",
 		RunE: func(ctx *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(ctx)
 			if err != nil {
@@ -62,6 +63,9 @@ func GetLeavesCmd() *cobra.Command {
 			queryClient := types.NewQueryClient(clientCtx)
 			params := &types.GetLeaveRequestsRequest{}
 			res, err := queryClient.GetLeaveRequests(ctx.Context(), params)
+			if err != nil {
+				panic(err)
+			}
 			return clientCtx.PrintProto(res)
 		},
 	}
@@ -72,9 +76,10 @@ func GetLeavesCmd() *cobra.Command {
 // Get Students Command
 func GetStudentsCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "students",
-		Short: "List all the students",
-		Long:  `List all the students which are added by`,
+		Use:     "students",
+		Short:   "Students",
+		Long:    `List all the students which are added by`,
+		Example: "./simd query coslms students",
 		RunE: func(ctx *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(ctx)
 			if err != nil {
@@ -96,9 +101,10 @@ func GetStudentsCmd() *cobra.Command {
 // Get Approved Leaves command
 func GetApprovedLeavesCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "approved-leaves",
-		Short: "List all approved leaves by admin",
-		Long:  `List all the approved leaves which are verified by admin`,
+		Use:     "approved-leaves",
+		Short:   "Approved Leaves",
+		Long:    `List all the approved leaves which are verified by admin`,
+		Example: "./simd query coslms approved-leaves",
 		RunE: func(ctx *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(ctx)
 			if err != nil {
@@ -107,26 +113,128 @@ func GetApprovedLeavesCmd() *cobra.Command {
 			queryClient := types.NewQueryClient(clientCtx)
 			params := &types.GetLeaveApprovedRequestsRequest{}
 			res, err := queryClient.GetLeaveApprovedRequests(ctx.Context(), params)
+			if err != nil {
+				panic(err)
+			}
 			return clientCtx.PrintProto(res)
 		},
 	}
 	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
-//Get Student Command
-func GetStudentCmd()*cobra.Command{
-	cmd:=&cobra.Command{
-		Use
+
+// Get Student Command
+func GetStudentCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "student  [address]",
+		Short:   "Student",
+		Long:    `Lists the student details based on the address input`,
+		Example: "./simd query coslms student [address]",
+		RunE: func(ctx *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(ctx)
+			if err != nil {
+				return err
+			}
+			student := types.GetStudentRequest{
+				Address: args[0],
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.GetStudent(ctx.Context(), &student)
+			if err != nil {
+				panic(err)
+			}
+			return clientCtx.PrintProto(res)
+		},
 	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// Get Admin Command
+func GetAdminCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "admin [address]",
+		Short:   "Admin",
+		Long:    `Lists the admin based on the address provided`,
+		Example: "./simd query coslms admin [address]",
+		RunE: func(ctx *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(ctx)
+			if err != nil {
+				return err
+			}
+			admin := types.GetAdminRequest{
+				Address: args[0],
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.GetAdmin(ctx.Context(), &admin)
+			if err != nil {
+				panic(err)
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// Get Admins Command
+func GetAdminsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "admins",
+		Short:   "Admins",
+		Long:    `List all the registered admins`,
+		Example: "./simd query coslms admins",
+		RunE: func(ctx *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(ctx)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			params := &types.GetAdminsRequest{}
+			res, err := queryClient.GetAdmins(ctx.Context(), params)
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+func GetLeaveStatusCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "leave-status [admin] [leaveid]",
+		Short:   "Leave Status",
+		Long:    `Get the leave status once the id and admin are passed`,
+		Example: "./simd query coslms leave-status [admin] [leaveid]",
+		RunE: func(ctx *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(ctx)
+			if err != nil {
+				panic(err)
+			}
+			status := types.GetLeaveStatusRequest{
+				Admin:   args[0],
+				LeaveId: args[1],
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.GetLeaveStatus(ctx.Context(), &status)
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
 }
 func init() {
 	rootCmd.AddCommand(GetStudentsCmd())
 	rootCmd.AddCommand(GetLeavesCmd())
 	rootCmd.AddCommand(GetApprovedLeavesCmd())
 	rootCmd.AddCommand(GetStudentCmd())
-	rootCmd.AddCommand(GetAdmins())
+	rootCmd.AddCommand(GetAdminsCmd())
 	rootCmd.AddCommand(GetLeaveStatusCmd())
-	rootCmd.AddCommand(GetAdmin())
+	rootCmd.AddCommand(GetAdminCmd())
 	rootCmd.SuggestionsMinimumDistance = 3
 
 	// Here you will define your flags and configuration settings.
